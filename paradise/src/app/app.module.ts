c663104962806668b1b1918dfa157b081f7f3164
benmendoza3 <b.mendoza@ufl.edu>
2023-08-14 12:32:53 -0400

    started frontend design

--- paradise/src/app/app.module.ts
+++ paradise/src/app/app.module.ts
@@ -7,12 +7,14 @@ import { HeaderComponent } from './header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomeComponent
+    HomeComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
