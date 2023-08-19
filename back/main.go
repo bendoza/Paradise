985f08b66ace8179fb7b5c5590b011de720b57e0
benmendoza3 <b.mendoza@ufl.edu>
2023-08-18 22:59:32 -0400

    documented back end

--- back/main.go
+++ back/main.go
@@ -18,45 +18,59 @@ import (
 	"github.com/joho/godotenv"
 )
 
+// Get Function for multiple calls, depending on range value specified by function parameter
+
 func GetData(w http.ResponseWriter, r *http.Request, value string) {
 
+	// Making sure the method is a GET request
 	if r.Method == "GET" {
 
+		// Attempt to load the environment file, terminate while logging error if fails
 		err := godotenv.Load(".env")
 		if err != nil {
     		log.Fatal("Error loading .env file")
 		}
 
+		// If not, load the key and spreadsheet ID from the env file
 		API := os.Getenv("googleApiKey")
 		ID := os.Getenv("spreadsheetId")
 
+		// Constructing URL of call via google sheets api for GET request with sheet ID, range value, and api key
 		url := "https://sheets.googleapis.com/v4/spreadsheets/" + ID + "/values/" + value + "?key=" + API
 
+		// Attempt get request on google sheets api, terminate and log fatal error if fails
 		response, err := http.Get(url)
 		if err != nil {
 			fmt.Println("Error:", err)
 			return
 		}
 
+		// Deferring the closing of the response body variable to whenever this (GetData) function exits
 		defer response.Body.Close()
 
+		// Initialize a variable for the response from the get request
 		var body []byte
+
+		// Attempt to read the response from the get request and store into variable initialized before
 		body, err = io.ReadAll(response.Body)
 		if err != nil {
 			fmt.Println("Error:", err)
 			return
 		}
 
+		// Initializing a 2d array to store the JSON response of the list of rows from the requested range
 		var data struct {
 			Values [][]string `json:"values"`
 		}
 
+		// Decoding the JSON data returned by the GET requesr
 		err = json.Unmarshal(body, &data)
 		if err != nil {
 			fmt.Println("Error:", err)
 			return
 		}
 
+		// Encode the values from the response variable into a json response to complete the clients request
 		json.NewEncoder(w).Encode(data.Values)
 
 	}
@@ -64,6 +78,7 @@ func GetData(w http.ResponseWriter, r *http.Request, value string) {
 
 func AddVisit(w http.ResponseWriter, r *http.Request) {
 
+	// Making sure the method is a PUT request
 	if r.Method == "PUT" {
 
 		err := godotenv.Load(".env")
@@ -76,6 +91,7 @@ func AddVisit(w http.ResponseWriter, r *http.Request) {
 
 		url := "https://sheets.googleapis.com/v4/spreadsheets/" + ID + "/values/ParadiseCraftDistributors!P2" + "?key=" + API
 
+		// Getting the current number of the visit count
 		response, err := http.Get(url)
 		if err != nil {
 			fmt.Println("Error:", err)
@@ -101,28 +117,34 @@ func AddVisit(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 
+		// Translate string value of the visit count into an integer
 		existingValue, err := strconv.Atoi(data.Values[0][0])
 		if err != nil {
 			fmt.Println("Error:", err)
 			return
 		}
 
+		// Add 1 to the current value of the visit count
 		incrementValue := 1
 		newValue := existingValue + incrementValue
 
+		// Initialize variable for google sheets api call
 		ctx := context.Background()
 
+		// Attempting to read google credentials for sheets api PUT call
 		serviceAccountKey := "/Users/bendoza/Desktop/paradise/Paradise/back/arctic-sound-388921-43b01ba0e3d1.json"
 		sa, err := os.ReadFile(serviceAccountKey)
 		if err != nil {
 			log.Fatalf("Error reading service account key: %v", err)
 		}
 
+		// Create config for the PUT call with credentials and google sheets api url
 		config, err := google.JWTConfigFromJSON(sa, "https://www.googleapis.com/auth/spreadsheets")
 		if err != nil {
 			log.Fatalf("Error creating JWT config: %v", err)
 		}
 
+		// Initializing google sheets api variable
 		client := config.Client(ctx)
 
 		sheetsService, err := sheets.NewService(ctx, option.WithHTTPClient(client))
@@ -130,20 +152,24 @@ func AddVisit(w http.ResponseWriter, r *http.Request) {
 			log.Fatalf("Unable to retrieve Sheets client: %v", err)
 		}
 
+		// Selecting cell with visit count value
 		rangeToUpdate := "ParadiseCraftDistributors!P2"
 
+		// Initializing the range of the sheet with the new value after increment
 		valueRange := sheets.ValueRange{
 			Values: [][]interface{}{
 				{newValue},
 			},
 		}
 
+		// PUT new value of the visit count after increment to the spreadsheet at the specified cell
 		_, err = sheetsService.Spreadsheets.Values.Update(ID, rangeToUpdate, &valueRange).
 			ValueInputOption("RAW").Do()
 		if err != nil {
 			log.Fatalf("Unable to update value: %v", err)
 		}
 
+		// Encode response to complete clients request
 		json.NewEncoder(w).Encode(map[string]interface{}{
 			"message": fmt.Sprintf("Value incremented by %d successfully", incrementValue),
 			"value":   newValue,
